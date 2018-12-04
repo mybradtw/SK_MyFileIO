8d2a206b6daa9865d438cdf9530b76c9a2b03d18
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2018-12-04 15:37:39 +0800

    v1

---
+++ app/src/main/java/tw/brad/myfileio/MainActivity.java
@@ -0,0 +1,67 @@
+package tw.brad.myfileio;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private File sdroot, approot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    },
+                    123);
+        }else{
+            init();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
+
+    private void init(){
+        sdroot = Environment.getExternalStorageDirectory();
+        Log.v("brad", sdroot.getAbsolutePath());
+
+        approot = new File(sdroot, "Android/data/" +
+                            getPackageName() + "/");
+        if (!approot.exists()){
+            Log.v("brad", "mkdir...");
+            approot.mkdirs();
+        }
+
+
+
+    }
+
+
+    public void test1(View view) {
+    }
+    public void test2(View view) {
+    }
+    public void test3(View view) {
+    }
+}
